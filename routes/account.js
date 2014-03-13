34ae491667caf2cb4a7e49e88a08b16d68192410
developer-4-giri <developer.4.giri@gmail.com>
2014-03-12 23:25:39 -0400

    added account and opportunity querry

--- routes/account.js
+++ routes/account.js
@@ -7,11 +7,23 @@ exports.getaccounts = function(org){
 		  return typeof element == "string";
 		});
 	
-		org.query({ query:'select id, name, ownerid  from account', oauth: req.session.oauth  }, function(err, resp){
-			  if(!err && resp.records) {
-					console.log(' Accounts :' + util.inspect(resp.records, { showHidden: false }));
-					res.render("accounts.html", { accounts: resp.records });
-			  } 
+		var query_string = 'select AccountNumber, Active__c, AnnualRevenue, Description, Id, Name, OwnerId, Type, (select AccountId, Amount, CloseDate, CreatedDate, Description, Id, IsClosed, IsDeleted, IsWon, LeadSource, Name, OwnerId, Pricebook2Id, StageName, Type from Opportunities) from Account';
+		org.query({ query:query_string, oauth: req.session.oauth  }, function(err, resp){
+			if(!err && resp.records) {
+				for(var i = 0; i < resp.records.length;i++){
+					var record = resp.records[i];
+					var porducts_bought = '';
+					
+					if(record._fields.opportunities != null){
+						for(var count = 0; count < record._fields.opportunities.records.length ; count++) {
+							console.log(' Opportunity Record:' + util.inspect(record._fields.opportunities.records[count], { showHidden: false }) );
+						}
+					}
+							
+				}
+			}
+				  
+			res.render("accounts.html", { accounts: resp.records });
 		});
 		
 	}
